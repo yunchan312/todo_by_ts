74dd4db55353fbd5ade302f10791bf332052a418
yunchan312 <phenomenal312@hufs.ac.kr>
2023-05-30 20:21:05 +0900

    props, optional props, state

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle borderColor="yellow" bgColor="teal" />
+      <Circle text="hello" bgColor="tomato" />
+    </div>
+  );
+}
+
+export default App;
